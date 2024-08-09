28bd8dfb72de14f4249bccbca60898c0e281d72c
lumendes10 <lucasmendes2379@gmail.com>
2024-08-09 12:03:16 -0300

    agencia

---
+++ back_end/agencia.py
@@ -0,0 +1,18 @@
+#vários argumentos (xargs) identificando o parametro
+
+#criar uma função que armazena numeors e strigs (dados)
+
+def agencia(**carro):
+    return carro
+print(agencia(marca='gol', cor='branco', motor=1.0, placa='stp 1234'))
+print(agencia(marca='celta', cor='preto', motor=1.0, placa='qvu 1280'))
+print(agencia(marca='civic', cor='azul', motor=1.8, placa='ksv 1430'))
+print(agencia(marca='polo', cor='verde', motor=1.5, placa='vuw 9837'))
+print(agencia(marca='onix', cor='branco', motor=1.0, placa='yha 3265'))
+print(agencia(marca='palio', cor='cinza', motor=1.4, placa='amt 0915'))
+print(agencia(marca='fiesta', cor='prata', motor=1.6, placa='eue 4j15'))
+print(agencia(marca='fusion', cor='preto', motor=2.0, placa='rmo 3570'))
+print(agencia(marca='agile', cor='marrom', motor=1.4, placa='sst 4215'))
+print(agencia(marca='prisma', cor='lilas', motor=1.4, placa= 'hka 4257'))
+print(agencia(marca='cronos', cor='laranja', motor=1.0, placa='thp 5298'))
+print(agencia(marca='pegout 206', cor='branca', motor=1.4, placa='fut 4267'))
